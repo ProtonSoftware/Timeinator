2d056baba8fa53191142c797f2dde2893127c9c8
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-10-12 10:18:53 +0200

    Update timeinator.sql

---
+++ timeinator.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE "db_timeinator";
+USE "db_timeinator";
+
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (
+    "id" int NOT NULL UNIQUE,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password" VARCHAR(255) NOT NULL,
+    "salt" VARCHAR(255) NOT NULL,
+    "token" VARCHAR(255) DEFAULT NULL,
+    "token_expiration" TIMESTAMP DEFAULT NULL,
+    PRIMARY KEY ("id")
+);
+
+DROP TABLE IF EXISTS "tasks";
+CREATE TABLE "tasks" (
+    "id" int NOT NULL UNIQUE,
+    "user" int NOT NULL,
+    "static" boolean DEFAULT 0,
+    "active" boolean DEFAULT 0,
+    "time" TIMESTAMP NOT NULL,
+    "duration" TIME DEFAULT NULL,
+    "priority" int NOT NULL,
+    "cycle" TIME DEFAULT NULL,
+    "progress" FLOAT DEFAULT 0,
+    "started" boolean DEFAULT 0,
+    PRIMARY KEY ("id")
+);
