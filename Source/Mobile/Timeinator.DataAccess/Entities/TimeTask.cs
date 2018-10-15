b9c46010680c35a76e2ce1276a1f09d54c954a9a
Patryk Mikulski <minorsonek@gmail.com>
2018-10-15 22:07:49 +0200

    Base entity framework setup, base view model, project management, new gitignore

---
+++ Source/Mobile/Timeinator.DataAccess/Entities/TimeTask.cs
@@ -0,0 +1,12 @@
+﻿using Timeinator.Core;
+
+namespace Timeinator.Mobile.DataAccess
+{
+    /// <summary>
+    /// The time task that came from user and is saved in the database
+    /// </summary>
+    public class TimeTask : BaseObject<int>
+    {
+        // TODO: Zadanie dla Michała :)
+    }
+}
