dccab32869566b46631cccea07c29c3859d65fe2
Patryk Mikulski <minorsonek@gmail.com>
2018-10-16 10:06:50 +0200

    Page mechanic setup, added menu, base pages, basic view models, base value converter etc

---
+++ Source/Mobile/Timeinator.Mobile/ViewModels/Application/ApplicationViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Timeinator.Mobile
+{
+    /// <summary>
+    /// The view model for main application state
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Indicates which page is shown currently in the application
+        /// </summary>
+        public ApplicationPage CurrentPage { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Navigates the application to specified page
+        /// And sets the initial view model for that page if provided
+        /// </summary>
+        /// <param name="page">The page to go to</param>
+        /// <param name="viewModel">The view model to set initially on page (if provided)</param>
+        public void GoToPage(ApplicationPage page, BaseViewModel viewModel = null)
+        {
+            // Change current page to specified one
+            CurrentPage = page;
+
+            // Set new credentials in page host
+            (App.Current.MainPage as PageHost).CurrentPageViewModel = viewModel;
+            (App.Current.MainPage as PageHost).CurrentPage = page;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Mobile/Timeinator.Mobile/Views/Base/BasePage.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Timeinator.Mobile
+{
+    /// <summary>
+    /// Base page for every page in this application to inherit from
+    /// </summary>
+    public class BasePage : ContentPage
+    {
+       
+    }
+}
