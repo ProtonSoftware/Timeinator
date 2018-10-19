2a4f2d20506a0491056c43b6f7825f1095d53588
Patryk Mikulski <minorsonek@gmail.com>
2018-10-19 14:24:35 +0200

    Added auto-mapper and timetaskmanager to the DI

---
+++ Source/Mobile/Timeinator.Mobile/Mappers/TimeTasksMapper.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Timeinator.Mobile.DataAccess;
+
+namespace Timeinator.Mobile
+{
+    /// <summary>
+    /// The mapper for TimeTasks objects
+    /// </summary>
+    public class TimeTasksMapper
+    {
+        #region Private Members
+
+        /// <summary>
+        /// AutoMapper configuration for this mapepr
+        /// </summary>
+        private readonly IMapper mMapper;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TimeTasksMapper()
+        {
+            // Configure the AutoMapper 
+            mMapper = new MapperConfiguration(config =>
+            {
+                // Create TimeTask entity to TimeTaskContext map
+                config.CreateMap<TimeTask, TimeTaskContext>();
+            })
+            // And create it afterwards
+            .CreateMapper();
+        }
+
+        #endregion
+
+        #region Mapping Methods
+
+        /// <summary>
+        /// Maps a <see cref="TimeTask"/> to a <see cref="TimeTaskContext"/> object
+        /// </summary>
+        /// <param name="entity">The <see cref="TimeTask"/> to map</param>
+        /// <returns><see cref="TimeTaskContext"/></returns>
+        public TimeTaskContext Map(TimeTask entity) => mMapper.Map<TimeTaskContext>(entity);
+
+        #endregion
+    }
+}
