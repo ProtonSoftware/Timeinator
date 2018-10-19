c2daeb25b0f240d481e15cb814e0d7367aeb1712
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-10-19 14:04:14 +0200

    mid-work manager

--- Source/Mobile/Timeinator.Mobile/Contexts/TimeTaskContext.cs
+++ Source/Mobile/Timeinator.Mobile/Contexts/TimeTaskContext.cs
@@ -1,9 +1,21 @@
-﻿namespace Timeinator.Mobile
+﻿using System;
+
+namespace Timeinator.Mobile
 {
     /// <summary>
     /// The context for time task to use as an object in this app
     /// </summary>
     public class TimeTaskContext
     {
+        /// <summary>
+        /// User
+        /// </summary>
+        public int OrderId { get; set; }
+        public float Progress { get; set; }
+        public bool Started { get; set; }
+        public int Priority { get; set; }
+        public bool Enabled { get; set; }
+        public bool Important { get; set; }
+        public TimeSpan Cycle { get; set; }
     }
 }
