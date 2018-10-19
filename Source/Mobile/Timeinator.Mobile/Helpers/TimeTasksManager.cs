9f241eb5e17c1ab7521bce6e4d6011dbba3130c8
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-10-19 14:04:14 +0200

    mid-work manager

--- Source/Mobile/Timeinator.Mobile/Helpers/TimeTasksManager.cs
+++ Source/Mobile/Timeinator.Mobile/Helpers/TimeTasksManager.cs
@@ -1,10 +1,24 @@
-﻿namespace Timeinator.Mobile
+﻿using System.Collections.Generic;
+
+namespace Timeinator.Mobile
 {
     /// <summary>
     /// The manager that handles time tasks interactions
     /// </summary>
     public class TimeTasksManager
     {
+        public List<TimeTaskContext> TaskContexts { get; set; }
 
+        #region Constructor
+        public TimeTasksManager(List<TimeTaskContext> timecontexts)
+        {
+            TaskContexts = timecontexts;
+        }
+        #endregion
+
+        public void tets()
+        {
+            
+        }
     }
 }
