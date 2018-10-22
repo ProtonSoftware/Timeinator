7807c34b9b0414607da99e58c12a76bdce4e6f22
michalgreszta <michalgreszta08@gmail.com>
2018-10-22 13:10:30 +0200

    Create UserTimeHandler

---
+++ Source/Mobile/Timeinator.Mobile/Helpers/UserTimeHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace Timeinator.Mobile
+{
+    /// <summary>
+    /// Manager that handles time within one user session
+    /// </summary>
+    public class UserTimeHandler
+    {
+        public List<TimeTaskContext> SessionTasks { get; set; }
+        public DateTime CurrentTime => DateTime.Now;
+        public DateTime CurrentTaskStartTime { get; set; }
+        public TimeSpan TimeLeft => CurrentTask.AssignedTime - (CurrentTime - CurrentTaskStartTime);
+        public TimeTaskContext CurrentTask => SessionTasks[0] ?? null;
+        public event Action TimesUp;
+
+        public void StartTask()
+        {
+            CurrentTaskStartTime = CurrentTime;
+            TimeLeft 
+        }
+
+        public void StopTask()
+        {
+
+        }
+
+        public void EndTask()
+        {
+
+        }
+
+        public void SaveProgress()
+        {
+
+        }
+}
