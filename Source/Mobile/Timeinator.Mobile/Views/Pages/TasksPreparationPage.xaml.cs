f8a04a4b0734b0fc3b157f7a7d83b5eeef06d2f6
Patryk Mikulski <minorsonek@gmail.com>
2018-10-30 17:23:26 +0100

    Plus icon, new task preparation page, UI changes

---
+++ Source/Mobile/Timeinator.Mobile/Views/Pages/TasksPreparationPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Timeinator.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TasksPreparationPage : BasePage
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TasksPreparationPage()
+        {
+            // Do default things
+            InitializeComponent();
+
+            // Set brand-new view model
+            BindingContext = new TasksPreparationViewModel();
+        }
+
+        /// <summary>
+        /// Constructor with additional view model to setup for this page
+        /// </summary>
+        public TasksPreparationPage(TasksPreparationViewModel viewModel)
+        {
+            // Do default things
+            InitializeComponent();
+
+            // Set specified view model
+            BindingContext = viewModel ?? new TasksPreparationViewModel();
+        }
+
+        #endregion
+    }
+}
