049d86dbd48c045233d8683100cb0b4b361b7bca
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-10-30 17:23:08 +0100

    sessionpage added

--- Source/Mobile/Timeinator.Mobile/ViewModels/Pages/SessionPageViewModel.cs
+++ Source/Mobile/Timeinator.Mobile/ViewModels/Pages/SessionPageViewModel.cs
@@ -1,13 +1,31 @@
-﻿namespace Timeinator.Mobile
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Timeinator.Mobile
 {
     /// <summary>
     /// The view model for tasks session page
     /// </summary>
     public class SessionPageViewModel : BaseViewModel
     {
+        #region PublicProperties
+
+        /// <summary>
+        /// The list of time tasks for current session to show in this page
+        /// </summary>
+        public ObservableCollection<TimeTaskViewModel> TaskItems { get; set; } = new ObservableCollection<TimeTaskViewModel>();
+        public bool Paused { get; set; }
+
+        #endregion
+
         #region Commands
 
-      
+        public ICommand BreakCommand { get; private set; }
+        public ICommand StopCommand { get; private set; }
+        public ICommand ExtendCommand { get; private set; }
+        public ICommand ResumeCommand { get; private set; }
 
         #endregion
 
@@ -18,9 +36,27 @@
         /// </summary>
         public SessionPageViewModel()
         {
-            
+            BreakCommand = new RelayCommand(() => DI.UserTimeHandler.StopTask());
+            StopCommand = new RelayCommand(() => DI.UserTimeHandler.EndTask());
+            //ExtendCommand = new RelayCommand(() => DI.UserTimeHandler.ExtendTask());
+            ResumeCommand = new RelayCommand(() => DI.UserTimeHandler.ResumeTask());
+            LoadTaskList();
+            List<TimeTaskContext> cxts = new List<TimeTaskContext>();
+            foreach (var task in TaskItems)
+                cxts.Add(DI.TimeTasksMapper.ReverseMap(task));
+            DI.UserTimeHandler.StartTimeHandler(cxts);
         }
 
         #endregion
+
+        /// <summary>
+        /// Loads saved tasks from the <see cref="TimeTasksManager"/>
+        /// </summary>
+        public void LoadTaskList()
+        {
+            // Get tasks from a manager
+            foreach (var task in DI.TimeTasksManager.TaskContexts)
+                TaskItems.Add(DI.TimeTasksMapper.Map(task));
+        }
     }
 }
