1a9fd4c1a7975816a11775f9994afdfbd746e0c3
Patryk Mikulski <minorsonek@gmail.com>
2018-11-20 23:08:23 +0100

    Added ParentedContentView and parentcontext mechanic to that, so child can access parent's view model

---
+++ Source/Mobile/Timeinator.Mobile/Views/Controls/XamarinExtensions/ParentedContentView.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace Timeinator.Mobile
+{
+    /// <summary>
+    /// An extension to the <see cref="ContentView"/> allowing it to store it's parent BindingContext to use
+    /// </summary>
+    public class ParentedContentView : ContentView
+    {
+        /// <summary>
+        /// The BindingContext of the parent of this view
+        /// </summary>
+        public object ParentContext
+        {
+            get => GetValue(ParentContextProperty);
+            set => SetValue(ParentContextProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ParentContext.  This enables animation, styling, binding, etc...
+        public static readonly BindableProperty ParentContextProperty =
+            BindableProperty.Create(nameof(ParentContext), typeof(object), typeof(ParentedContentView));
+    }
+}
