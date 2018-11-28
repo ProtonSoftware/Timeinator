d697e1fa6c72b7d602b74139ced516d909204ce7
Patryk Mikulski <minorsonek@gmail.com>
2018-11-28 22:15:09 +0100

    Migration system in database

---
+++ Source/Mobile/Timeinator.DataAccess/Migrations/20181128211351_TaskDatesAndSettingsType.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timeinator.Mobile.DataAccess.Migrations
+{
+    public partial class TaskDatesAndSettingsType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Priority = table.Column<int>(nullable: false),
+                    IsImportant = table.Column<bool>(nullable: false),
+                    Progress = table.Column<float>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Tag = table.Column<string>(nullable: true),
+                    IsImmortal = table.Column<bool>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    TargetStartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeTasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+
+            migrationBuilder.DropTable(
+                name: "TimeTasks");
+        }
+    }
+}
