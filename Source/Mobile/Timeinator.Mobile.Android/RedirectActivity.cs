8c95a0dab8c013695a0a51dab322970b5ddbf136
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-12-17 21:15:49 +0100

    Bringin up test notification

---
+++ Source/Mobile/Timeinator.Mobile.Android/RedirectActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Timeinator.Mobile.Droid
+{
+    [Activity(Label = "Redirecting")]
+    public class RedirectActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            DI.Application.GoToPage(ApplicationPage.TasksSession);
+            //base.OnCreate(savedInstanceState);
+        }
+    }
+}
