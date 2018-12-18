36dae800c6ea477560cfab34241418993dc706f8
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-12-18 10:57:31 +0100

    DI for Notifications

---
+++ Source/Mobile/Timeinator.Mobile.Android/Notification/NotificationHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Timeinator.Mobile.Droid
+{
+    /// <summary>
+    /// Notification handling for Android
+    /// </summary>
+    public class NotificationHandler : INotificationHandler
+    {
+        private static readonly int NOTIFICATION_ID = 3030;
+        private static readonly string CHANNEL_ID = "session_notification";
+
+        /// <summary>
+        /// Builds basic notification redirecting to session page and shows it
+        /// </summary>
+        public void BuildNotification(string title, string content)
+        {
+            //var intent = new Intent(this, typeof(MainActivity));
+            //const int pendingIntentId = 0;
+            //var pendingIntent = PendingIntent.GetActivity(this, pendingIntentId, intent, PendingIntentFlags.OneShot);
+            var builder = new Android.Support.V4.App.NotificationCompat.Builder(this, CHANNEL_ID)
+                            //.SetContentIntent(pendingIntent)
+                            .SetContentTitle(title)
+                            .SetDefaults((int)NotificationDefaults.All)
+                            .SetSmallIcon(Resource.Drawable.notification_template_icon_bg)
+                            .SetContentText(content);
+            //var notificationManager = Android.Support.V4.App.NotificationManagerCompat.From(this);
+            var notificationManager = GetSystemService(Context.NotificationService) as NotificationManager;
+            notificationManager.Notify(NOTIFICATION_ID, builder.Build());
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O) // required only since API 26
+                return;
+            var channelName = "Timeinator";
+            var channelDescription = "Timeinator session notifications";
+            var channel = new NotificationChannel(CHANNEL_ID, channelName, NotificationImportance.High)
+            {
+                Description = channelDescription,
+                LockscreenVisibility = NotificationVisibility.Public
+            };
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+    }
+}
---
+++ Source/Mobile/Timeinator.Mobile/DI/Interfaces/INotificationHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timeinator.Mobile
+{
+    public interface INotificationHandler
+    {
+        void UploadTasksList(List<TimeTaskContext> contexts, TimeSpan userTime);
+
+        List<TimeTaskContext> GetCalculatedTasksListForSpecifiedTime();
+    }
+}
