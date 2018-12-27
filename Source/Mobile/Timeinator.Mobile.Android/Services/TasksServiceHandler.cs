e2e0d16abc10d6098de904bdd8e70e6136a3ccdb
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-12-27 18:14:32 +0100

    Preparing AndroidService to move Handler functionality

---
+++ Source/Mobile/Timeinator.Mobile.Android/Services/TasksServiceHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Timeinator.Mobile.Droid
+{
+    public class TasksServiceHandler : ITasksServiceHandler
+    {
+        #region Private members
+
+        private TaskIntentService mCurrentTaskSvc;
+
+        #endregion
+
+        #region Interface implementation
+
+        public void NewTask(int assignedTime)
+        {
+            mCurrentTaskSvc = new TaskIntentService(assignedTime);
+            mCurrentTaskSvc.StartForegroundService(new Intent());
+        }
+
+        public void StopTask()
+        {
+            mCurrentTaskSvc.StopSelf();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Mobile/Timeinator.Mobile/DI/Interfaces/ITasksServiceHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timeinator.Mobile
+{
+    public interface ITasksServiceHandler
+    {
+    }
+}
