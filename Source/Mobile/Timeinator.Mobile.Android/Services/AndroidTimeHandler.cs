35c4dbf46959dc990898ce4387902a5d4a6951ff
GummyBearStudio <44005871+GummyBearStudio@users.noreply.github.com>
2018-12-28 19:16:12 +0100

    Preparing Android Service classes

--- Source/Mobile/Timeinator.Mobile.Android/Services/AndroidTimeHandler.cs
+++ Source/Mobile/Timeinator.Mobile.Android/Services/AndroidTimeHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Timers;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace Timeinator.Mobile.Droid
 {
-    public class TasksServiceHandler : ITasksServiceHandler
+    public class AndroidTimeHandler : UserTimeHandler
     {
         #region Private members
 
@@ -22,15 +22,20 @@ namespace Timeinator.Mobile.Droid
 
         #region Interface implementation
 
-        public void NewTask(int assignedTime)
+        public override void StartTimeHandler(List<TimeTaskContext> sessionTasks)
         {
-            mCurrentTaskSvc = new TaskIntentService(assignedTime);
-            mCurrentTaskSvc.StartForegroundService(new Intent());
+            TaskTimer = new Timer
+            {
+                AutoReset = false
+            };
+            SessionTasks = new List<TimeTaskContext>(sessionTasks);
+            CurrentTaskStartTime = CurrentTime;
+            StartTask();
         }
 
-        public void StopTask()
+        public override void StartTask()
         {
-            mCurrentTaskSvc.StopSelf();
+            base.StartTask();
         }
 
         #endregion
--- Source/Mobile/Timeinator.Mobile.Android/Services/TaskIntentService.cs
+++ Source/Mobile/Timeinator.Mobile.Android/Services/TaskIntentService.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Java.Util;
 
 namespace Timeinator.Mobile.Droid
 {
     [Service]
     public class TaskIntentService : IntentService
     {
-        private int SleepTime { get; set; }
+        private static Timer TaskTimer = new Timer();
+        private TimeSpan TaskTime { get; set; }
 
-        public TaskIntentService(int assignedTime) : base("TaskIntentService")
+        public TaskIntentService(DateTime assignedTime) : base("TaskIntentService")
         {
-            SleepTime = assignedTime;
+            TaskTime = DateTime.Now - assignedTime;
         }
 
         protected override void OnHandleIntent(Intent intent)
         {
-            Thread.Sleep(SleepTime);
+            if (intent.Action == IntentActions.ACTION_NEXTTASK)
+                return;
         }
     }
 }
