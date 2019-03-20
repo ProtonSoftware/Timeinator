85bd718fa961bc25b971e66a708ebeb5776271d1
Minorsonek <minorsonek@gmail.com>
2019-03-20 19:35:00 +0100

    Fully working initial support for mvvm in AndroidNative project

---
+++ Source/Mobile/Timeinator.Mobile.Core/App.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using System;
+
+namespace Timeinator.Mobile.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+            RegisterAppStart<LoginPageViewModel>();
+        }
+    }
+}
