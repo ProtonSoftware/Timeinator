1f6909baef6362a6c038667e65c52e470cd82174
Minorsonek <minorsonek@gmail.com>
2019-03-20 19:35:00 +0100

    Fully working initial support for mvvm in AndroidNative project

---
+++ Source/Mobile/Timeinator.Mobile.AndroidNative/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platforms.Android.Core;
+using Timeinator.Mobile.Core;
+
+namespace Timeinator.Mobile.AndroidNative
+{
+    [Activity(
+        Label = "$rootnamespace$"
+        , MainLauncher = true
+        , NoHistory = true
+        , Theme = "@style/AppTheme"
+        , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenAppCompatActivity<MvxAppCompatSetup<App>, App>
+    {
+        public SplashScreen()
+             : base(Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
---
+++ Source/Mobile/Timeinator.Mobile.Core/ViewModels/Pages/LoginPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MvvmCross.ViewModels;
+using System.Windows.Input;
+
+namespace Timeinator.Mobile.Core
+{
+    /// <summary>
+    /// The view model for initial login page
+    /// </summary>
+    public class LoginPageViewModel : MvxViewModel
+    {
+        public string Temp { get; set; } = "COOOOS";
+        #region Commands
+
+        /// <summary>
+        /// The command to enter the application without logging in
+        /// </summary>
+        public ICommand EnterWithoutLoginCommand { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public LoginPageViewModel()
+        {
+            // Create commands
+            EnterWithoutLoginCommand = new RelayCommand(ChangePageWithoutLogin);
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// Changes the page without users' logging in
+        /// </summary>
+        public void ChangePageWithoutLogin()
+        {
+            Temp += "C";
+        }
+
+        #endregion
+    }
+}
