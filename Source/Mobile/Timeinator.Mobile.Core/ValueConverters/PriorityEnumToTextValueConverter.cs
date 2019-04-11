8b087f72117cca12d3bb04a51ef708fb12df69ab
Minorsonek <minorsonek@gmail.com>
2019-04-11 17:47:20 +0200

    Added value converters so task list item shows all the informations correctly and in friendly way

---
+++ Source/Mobile/Timeinator.Mobile.Core/ValueConverters/PriorityEnumToTextValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using MvvmCross.Converters;
+using System;
+using System.Globalization;
+using Timeinator.Core;
+
+namespace Timeinator.Mobile.Core
+{
+    /// <summary>
+    /// The value converter for task's priority to show it as text instead of enum name
+    /// </summary>
+    public class PriorityEnumToTextValueConverter : IMvxValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var priority = (Priority)value;
+
+            return $"P: {(int)priority}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
