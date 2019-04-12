892a3716a4675a5a5c3ee45d6b94f024992272eb
Minorsonek <minorsonek@gmail.com>
2019-04-12 15:40:46 +0200

    Sample settings and about page

---
+++ Source/Mobile/Timeinator.Mobile.AndroidNative/Views/SettingsPage.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Timeinator.Mobile.AndroidNative
+{
+    [MvxActivityPresentation]
+    [Activity(Label = "View for SettingsPageViewModel")]
+    public class SettingsPage : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.SettingsPage);
+        }
+    }
+}
