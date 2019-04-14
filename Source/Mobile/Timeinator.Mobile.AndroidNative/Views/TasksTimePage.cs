2c063c01f154fbfbbfaf661396ee60818830ac0e
Minorsonek <minorsonek@gmail.com>
2019-04-14 13:53:24 +0200

    Better elevation UI, combobox done, session time text

--- Source/Mobile/Timeinator.Mobile.AndroidNative/Views/TasksTimePage.cs
+++ Source/Mobile/Timeinator.Mobile.AndroidNative/Views/TasksTimePage.cs
@@ -7,7 +7,8 @@ using MvvmCross.Platforms.Android.Views;
 namespace Timeinator.Mobile.AndroidNative
 {
     [MvxActivityPresentation]
-    [Activity(Label = "View for TasksTimePageViewModel")]
+    [Activity(Label = "View for TasksTimePageViewModel",
+              NoHistory = true)]
     public class TasksTimePage : MvxActivity
     {
         protected override void OnCreate(Bundle bundle)
