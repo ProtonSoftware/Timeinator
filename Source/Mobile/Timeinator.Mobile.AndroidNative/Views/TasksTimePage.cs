d5e661a6e6efbe74c6a02c82aab860c8f1393db5
GummyBearStudio <keyboardwarriror@gmail.com>
2019-04-21 19:58:35 +0200

    TimePicker Binding

--- Source/Mobile/Timeinator.Mobile.AndroidNative/Views/TasksTimePage.cs
+++ Source/Mobile/Timeinator.Mobile.AndroidNative/Views/TasksTimePage.cs
@@ -1,11 +1,8 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.OS;
 using Android.Widget;
-using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
-using Timeinator.Mobile.Core;
 
 namespace Timeinator.Mobile.AndroidNative
 {
@@ -18,28 +15,6 @@ namespace Timeinator.Mobile.AndroidNative
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.TasksTimePage);
-
-            var tp = (TimePicker)FindViewById(Resource.Id.pickerSession);
-            tp.SetIs24HourView((Java.Lang.Boolean)true);
-            mOnTimeChanged = new TimeChanged();
-            tp.SetOnTimeChangedListener(mOnTimeChanged);
-
-            var set = this.CreateBindingSet<TasksTimePage, TasksTimePageViewModel>();
-            set.Bind(mOnTimeChanged).For(t => t.SessionTime).To(vm => vm.UserTime);
-            set.Apply();
-        }
-
-        private TimeChanged mOnTimeChanged;
-
-        [PropertyChanged.AddINotifyPropertyChangedInterface]
-        private class TimeChanged : Java.Lang.Object, TimePicker.IOnTimeChangedListener
-        {
-            public System.TimeSpan SessionTime { get; set; }
-
-            public void OnTimeChanged(TimePicker view, int hour, int minute)
-            {
-                SessionTime = new System.TimeSpan(hour, minute, 0);
-            }
         }
     }
 }
