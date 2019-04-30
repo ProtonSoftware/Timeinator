3727bc4d50b518b76a55532d11cb9e9304564667
GummyBearStudio <keyboardwarriror@gmail.com>
2019-04-30 14:04:39 +0200

    Update ProgressValueConverter.cs

--- Source/Mobile/Timeinator.Mobile.Core/ValueConverters/ProgressValueConverter.cs
+++ Source/Mobile/Timeinator.Mobile.Core/ValueConverters/ProgressValueConverter.cs
@@ -11,11 +11,11 @@ namespace Timeinator.Mobile.Core
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var progress = (double)value * 100;
+            var progress = (int)((double)value * 100);
             if (parameter != null)
-                return string.Format("{0:0.00}%", progress);
+                return $"{progress}%";
             else
-                return (int)progress;
+                return progress;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
