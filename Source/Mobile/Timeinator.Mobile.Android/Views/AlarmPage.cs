f78cbd3c81f07e4b1c0e4faa4dbea89b7127a2a0
GummyBearStudio <keyboardwarriror@gmail.com>
2019-05-20 16:29:59 +0200

    Prepared files for AlarmPage

---
+++ Source/Mobile/Timeinator.Mobile.Android/Views/AlarmPage.cs
@@ -0,0 +1,26 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Timeinator.Mobile.Android
+{
+    [MvxActivityPresentation]
+    [Activity(Label = "View for AlarmPageViewModel")]
+    public class AlarmPage : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.TasksSessionPage);
+
+            OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out);
+        }
+    }
+}
