3376e80e3647549022ad48938ab750399a2e39a2
Minorsonek <minorsonek@gmail.com>
2019-06-16 17:12:07 +0200

    Working checkbox settings

--- Source/Mobile/Timeinator.Mobile.Android/Views/SettingsPage.cs
+++ Source/Mobile/Timeinator.Mobile.Android/Views/SettingsPage.cs
@@ -1,7 +1,12 @@
 ﻿using Android.App;
 using Android.OS;
+using MvvmCross;
+using MvvmCross.Binding.Binders;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
+using System.Linq;
+using Timeinator.Core;
+using Timeinator.Mobile.Core;
 
 namespace Timeinator.Mobile.Android
 {
@@ -15,6 +20,32 @@ namespace Timeinator.Mobile.Android
             SetContentView(Resource.Layout.SettingsPage);
 
             OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            var viewModel = BindingContext.DataContext as SettingsPageViewModel;
+            var binder = Mvx.IoCProvider.Resolve<IMvxBinder>();
+
+            // Create a fragment for every setting that will be shown in this page
+            var settingHighestPriorityFragment = new SettingsCheckboxFragment(LocalizationResource.SettingHighestPriorityName, LocalizationResource.SettingHighestPriorityDescription);
+            var settingRecalculateTasksFragment = new SettingsCheckboxFragment(LocalizationResource.SettingRecalculateTasksName, LocalizationResource.SettingRecalculateTasksDescription);
+
+            // Show them in the view
+            FragmentManager.BeginTransaction()
+                .Replace(Resource.Id.settingHighestPriority, settingHighestPriorityFragment)
+                .Replace(Resource.Id.settingRecalculateTasks, settingRecalculateTasksFragment)
+                .AddToBackStack(null)
+                .Commit();
+            FragmentManager.ExecutePendingTransactions();
+
+            var bindingHighestPriority = binder.Bind(viewModel, settingHighestPriorityFragment.CheckBoxView, "Checked HighestPrioritySetAsFirst").First();
+            BindingContext.RegisterBinding(settingHighestPriorityFragment.CheckBoxView, bindingHighestPriority);
+
+            var bindingRecalculateTasks = binder.Bind(viewModel, settingRecalculateTasksFragment.CheckBoxView, "Checked RecalculateTasksAfterBreak").First();
+            BindingContext.RegisterBinding(settingRecalculateTasksFragment.CheckBoxView, bindingRecalculateTasks);
         }
     }
 }
