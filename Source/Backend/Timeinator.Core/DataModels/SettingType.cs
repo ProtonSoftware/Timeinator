66cc11d9a513a30d7e408ad7f54ba8f0f397e15f
Minorsonek <minorsonek@gmail.com>
2019-06-17 20:46:52 +0200

    Dropdown setting fragment done

---
+++ Source/Backend/Timeinator.Core/DataModels/SettingType.cs
@@ -0,0 +1,18 @@
+﻿namespace Timeinator.Core
+{
+    /// <summary>
+    /// Every possible setting type for the final user
+    /// </summary>
+    public enum SettingType
+    {
+        /// <summary>
+        /// The setting with toggleable value (on or off)
+        /// </summary>
+        Toggleable,
+
+        /// <summary>
+        /// The setting with multiple values to choose from
+        /// </summary>
+        MultipleValues
+    }
+}
