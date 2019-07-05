d5cce9c1a3d8181f1f764b280ac41cbc97758dcf
Minorsonek <minorsonek@gmail.com>
2019-07-05 10:05:37 +0200

    Working wake lock that keeps CPU alive in the background, so session is still working

--- Source/Mobile/Timeinator.Mobile.Android/Views/TasksSessionPage.cs
+++ Source/Mobile/Timeinator.Mobile.Android/Views/TasksSessionPage.cs
@@ -1,5 +1,5 @@
-﻿
-using Android.App;
+﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
@@ -10,12 +10,40 @@ namespace Timeinator.Mobile.Android
     [Activity(Label = "View for TasksSessionPageViewModel")]
     public class TasksSessionPage : MvxActivity
     {
+        #region Private Members
+
+        /// <summary>
+        /// The android wake lock feature that allows to keep CPU running in the background
+        /// </summary>
+        private PowerManager.WakeLock mWakeLock;
+
+        #endregion
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.TasksSessionPage);
 
             OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            // Get android power manager
+            var powerManager = Application.Context.GetSystemService(Context.PowerService) as PowerManager;
+
+            // Setup new wake lock that allows to keep CPU running in the background
+            mWakeLock = powerManager.NewWakeLock(WakeLockFlags.Partial, "WakeLockTag");
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            // Use prepared wake lock to prevent CPU pause when app is closed
+            mWakeLock.Acquire();
         }
     }
 }
