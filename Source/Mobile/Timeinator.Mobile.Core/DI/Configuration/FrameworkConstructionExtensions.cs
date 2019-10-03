49a9abb7ceba8b1723d2502c5516fe5fa5c415de
Minorsonek <minorsonek@gmail.com>
2019-10-03 11:14:27 +0200

    Linker work for release x64 mode, this is the working state Too many libraries are skipped currently, so most of them should be removed now

--- Source/Mobile/Timeinator.Mobile.Core/DI/Configuration/FrameworkConstructionExtensions.cs
+++ Source/Mobile/Timeinator.Mobile.Core/DI/Configuration/FrameworkConstructionExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Dna;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Timeinator.Mobile.DataAccess;
 
 namespace Timeinator.Mobile.Core
@@ -10,6 +13,46 @@ namespace Timeinator.Mobile.Core
     /// </summary>
     public static class FrameworkConstructionExtensions
     {
+        private static readonly Dictionary<MethodInfo, string> MethodInfoToUnitSuffix = new Dictionary<MethodInfo, string>
+        {
+            { typeof(DateTime).GetRuntimeMethod(nameof(DateTime.AddYears), new[] { typeof(int) }), " years" },
+            { typeof(DateTime).GetRuntimeMethod(nameof(DateTime.AddMonths), new[] { typeof(int) }), " months" },
+            { typeof(DateTime).GetRuntimeMethod(nameof(DateTime.AddDays), new[] { typeof(double) }), " days" },
+            { typeof(DateTime).GetRuntimeMethod(nameof(DateTime.AddHours), new[] { typeof(double) }), " hours" },
+            { typeof(DateTime).GetRuntimeMethod(nameof(DateTime.AddMinutes), new[] { typeof(double) }), " minutes" },
+            { typeof(DateTime).GetRuntimeMethod(nameof(DateTime.AddSeconds), new[] { typeof(double) }), " seconds" }
+        };
+
+        private static readonly Dictionary<MethodInfo, string> SupportedMethods = new Dictionary<MethodInfo, string>
+        {
+            { typeof(Math).GetMethod(nameof(Math.Abs), new[] { typeof(double) }), "abs" },
+            { typeof(Math).GetMethod(nameof(Math.Abs), new[] { typeof(float) }), "abs" },
+            { typeof(Math).GetMethod(nameof(Math.Abs), new[] { typeof(int) }), "abs" },
+            { typeof(Math).GetMethod(nameof(Math.Abs), new[] { typeof(long) }), "abs" },
+            { typeof(Math).GetMethod(nameof(Math.Abs), new[] { typeof(sbyte) }), "abs" },
+            { typeof(Math).GetMethod(nameof(Math.Abs), new[] { typeof(short) }), "abs" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(byte), typeof(byte) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(double), typeof(double) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(float), typeof(float) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(int), typeof(int) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(long), typeof(long) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(sbyte), typeof(sbyte) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(short), typeof(short) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(uint), typeof(uint) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Max), new[] { typeof(ushort), typeof(ushort) }), "max" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(byte), typeof(byte) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(double), typeof(double) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(float), typeof(float) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(int), typeof(int) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(long), typeof(long) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(sbyte), typeof(sbyte) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(short), typeof(short) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(uint), typeof(uint) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Min), new[] { typeof(ushort), typeof(ushort) }), "min" },
+            { typeof(Math).GetMethod(nameof(Math.Round), new[] { typeof(double) }), "round" },
+            { typeof(Math).GetMethod(nameof(Math.Round), new[] { typeof(double), typeof(int) }), "round" }
+        };
+
         /// <summary>
         /// Injects the view models needed for Timeinator mobile application
         /// </summary>
