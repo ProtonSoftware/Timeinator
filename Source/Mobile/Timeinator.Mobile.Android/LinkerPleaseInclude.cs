3d86049c2448635753672d3af595765b9610c81d
Minorsonek <minorsonek@gmail.com>
2019-10-03 18:14:31 +0200

    LINKER WORKS!~!!

--- Source/Mobile/Timeinator.Mobile.Android/LinkerPleaseInclude.cs
+++ Source/Mobile/Timeinator.Mobile.Android/LinkerPleaseInclude.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-
+using System.Windows.Input;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.IoC;
+using MvvmCross.Navigation;
+using MvvmCross.Platforms.Android.Binding.Views;
+using MvvmCross.ViewModels;
+using MvvmCross.WeakSubscription;
 
 namespace Timeinator.Mobile.Android
 {
@@ -55,5 +65,182 @@ namespace Timeinator.Mobile.Android
             { typeof(Math).GetMethod(nameof(Math.Round), new[] { typeof(double) }), "round" },
             { typeof(Math).GetMethod(nameof(Math.Round), new[] { typeof(double), typeof(int) }), "round" }
         };
+
+        public void Include(Button button)
+        {
+            button.Click += (s, e) => button.Text = button.Text + "";
+        }
+
+        public void Include(CheckBox checkBox)
+        {
+            checkBox.CheckedChange += (sender, args) => checkBox.Checked = !checkBox.Checked;
+        }
+
+        public void Include(Switch @switch)
+        {
+            @switch.CheckedChange += (sender, args) => @switch.Checked = !@switch.Checked;
+        }
+
+        public void Include(FloatingActionButton button)
+        {
+            button = new FloatingActionButton(null);
+            button.Click += (s, e) => button.TooltipText = "";
+        }
+
+        public void Include(View view)
+        {
+            view.Click += (s, e) => view.ContentDescription = view.ContentDescription + "";
+        }
+
+        public void Include(TextView text)
+        {
+            text.AfterTextChanged += (sender, args) => text.Text = "" + text.Text;
+            text.Hint = "" + text.Hint;
+        }
+
+        public void Include(CheckedTextView text)
+        {
+            text.AfterTextChanged += (sender, args) => text.Text = "" + text.Text;
+            text.Hint = "" + text.Hint;
+        }
+
+        public void Include(CompoundButton cb)
+        {
+            cb.CheckedChange += (sender, args) => cb.Checked = !cb.Checked;
+        }
+
+        public void Include(SeekBar sb)
+        {
+            sb.ProgressChanged += (sender, args) => sb.Progress = sb.Progress + 1;
+        }
+
+        public void Include(Activity act)
+        {
+            act.Title = act.Title + "";
+        }
+
+        public void Include(MvxLinearLayout mvxLinearLayout)
+        {
+            mvxLinearLayout = new MvxLinearLayout(null, null);
+        }
+
+        public void Include(MvxRecyclerView mvxLinearLayout)
+        {
+            mvxLinearLayout = new MvxRecyclerView(null, null);
+            mvxLinearLayout.ChildViewAdded += (s, e) => mvxLinearLayout = null;
+            mvxLinearLayout.ViewAttachedToWindow += (s, e) => mvxLinearLayout = null;
+
+            mvxLinearLayout.OnChildAttachedToWindow(null);
+        }
+
+        public void Include(RecyclerView mvxLinearLayout)
+        {
+            mvxLinearLayout = new RecyclerView(null, null);
+            mvxLinearLayout.ChildViewAdded += (s, e) => mvxLinearLayout = null;
+            mvxLinearLayout.ViewAttachedToWindow += (s, e) => mvxLinearLayout = null;
+
+            mvxLinearLayout.OnChildAttachedToWindow(null);
+        }
+
+        public void Include(MvvmCross.Droid.Support.V7.RecyclerView.ItemTemplates.MvxDefaultTemplateSelector injector)
+        {
+            injector = new MvvmCross.Droid.Support.V7.RecyclerView.ItemTemplates.MvxDefaultTemplateSelector();
+        }
+
+        public void Include(IMvxRecyclerViewHolder viewHolder)
+        {
+            void OnItemViewClick(object sender, EventArgs e) { }
+            void OnItemViewLongClick(object sender, EventArgs e) { }
+
+            viewHolder.Click -= OnItemViewClick;
+            viewHolder.LongClick -= OnItemViewLongClick;
+            viewHolder.Click += OnItemViewClick;
+            viewHolder.LongClick += OnItemViewLongClick;
+        }
+
+        public void Include(RecyclerView.ViewHolder vh, MvxRecyclerView list)
+        {
+            vh.ItemView.Click += (sender, args) => { };
+            vh.ItemView.LongClick += (sender, args) => { };
+            list.ItemsSource = null;
+            list.Click += (sender, args) => { };
+        }
+
+        public void Include(MvxWeakEventSubscription<RecyclerView, RecyclerView> @event)
+        {
+            @event.WeakSubscribe(null, null);
+        }
+
+        public void Include(INotifyCollectionChanged changed)
+        {
+            changed.CollectionChanged += (s, e) => {
+                var test =
+$"{e.Action}{e.NewItems}{e.NewStartingIndex}{e.OldItems}{e.OldStartingIndex}";
+            };
+        }
+
+        public void Include(ICommand command)
+        {
+            command.CanExecuteChanged += (s, e) => { if (command.CanExecute(null)) command.Execute(null); };
+        }
+
+        public void Include(MvxPropertyInjector injector)
+        {
+            injector = new MvxPropertyInjector();
+        }
+
+        public void Include(System.ComponentModel.INotifyPropertyChanged changed)
+        {
+            changed.PropertyChanged += (sender, e) =>
+            {
+                var test = e.PropertyName;
+            };
+        }
+
+        public void Include(RadioGroup radioGroup)
+        {
+            radioGroup.CheckedChange += (sender, args) => radioGroup.Check(args.CheckedId);
+        }
+
+        public void Include(RadioButton radioButton)
+        {
+            radioButton.CheckedChange += (sender, args) => radioButton.Checked = args.IsChecked;
+        }
+
+        public void Include(RatingBar ratingBar)
+        {
+            ratingBar.RatingBarChange += (sender, args) => ratingBar.Rating = 0 + ratingBar.Rating;
+        }
+
+        public void Include(MvxTaskBasedBindingContext context)
+        {
+            context.Dispose();
+            var context2 = new MvxTaskBasedBindingContext();
+            context2.Dispose();
+        }
+
+        public void Include(MvxNavigationService service, IMvxViewModelLoader loader)
+        {
+            service = new MvxNavigationService(null, loader);
+        }
+
+        public void Include(MvxListView listview)
+        {
+            listview.ItemsSource = new List<int>();
+            var itemsSource = listview.ItemsSource;
+        }
+
+        public void Include(ConsoleColor color)
+        {
+            Console.Write("");
+            Console.WriteLine("");
+            color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
     }
 }
