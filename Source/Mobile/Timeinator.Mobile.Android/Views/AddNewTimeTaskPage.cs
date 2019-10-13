aa40a52ab840da58d8c055e1217aa12f56b2dc32
Minorsonek <minorsonek@gmail.com>
2019-10-13 11:03:14 +0200

    Added fragments support in AddNewTimeTask page that will be used for different task types

--- Source/Mobile/Timeinator.Mobile.Android/Views/AddNewTimeTaskPage.cs
+++ Source/Mobile/Timeinator.Mobile.Android/Views/AddNewTimeTaskPage.cs
@@ -1,94 +1,50 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.OS;
-using Android.Widget;
+using Android.Support.V4.View;
+using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
-using MvvmCross.Platforms.Android.Views;
-using Org.Adw.Library.Widgets.Discreteseekbar;
-using System;
+using System.Collections.Generic;
+using Timeinator.Core;
 using Timeinator.Mobile.Core;
-using static Android.App.TimePickerDialog;
 
 namespace Timeinator.Mobile.Android
 {
     [MvxActivityPresentation]
     [Activity(Label = "View for AddNewTimeTaskPageViewModel")]
-    public class AddNewTimeTaskPage : MvxActivity, IOnTimeSetListener
+    public class AddNewTimeTaskPage : MvxAppCompatActivity
     {
-        /// <summary>
-        /// Gets currently shown view model in this page
-        /// </summary>
-        private AddNewTimeTaskPageViewModel CurrentViewModel => BindingContext.DataContext as AddNewTimeTaskPageViewModel;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.AddNewTimeTaskPage);
 
             OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out);
+
+            // Get the view model for this page
+            var viewModel = BindingContext.DataContext as AddNewTimeTaskPageViewModel;
+
+            // Create the fragment pages for every question type
+            var fragments = new List<MvxFragmentInfo>
+            {
+                new MvxFragmentInfo
+                {
+                    FragmentType = typeof(AddNewTimeTaskFragment),
+                    Title = TimeTaskType.Generic.ToString(),
+                    ViewModel = viewModel
+                },
+                new MvxFragmentInfo
+                {
+                    FragmentType = typeof(AddNewTimeTaskFragment),
+                    Title = TimeTaskType.Reading.ToString(),
+                    ViewModel = viewModel
+                }
+            };
+
+            // Get the view pager from this page
+            var viewPager = FindViewById<ViewPager>(Resource.Id.viewPagerAdd);
+
+            // Set the content as fragments using our adapter
+            viewPager.Adapter = new MvxFragmentStatePagerAdapter(BaseContext, SupportFragmentManager, fragments);
         }
-
-        protected override void OnStart()
-        {
-            base.OnStart();
-
-            // Find main page view
-            var pageView = FindViewById(Resource.Id.addTaskPage);
-
-            // Find the priority seekbar
-            var prioritySeekBar = (DiscreteSeekBar)pageView.FindViewById(Resource.Id.prioritySeekBar);
-            
-            // Set initial progress value
-            prioritySeekBar.Progress = CurrentViewModel.TaskPrioritySliderValue;
-
-            // Listen out for future progress changes
-            prioritySeekBar.ProgressChanged += PrioritySeekBar_ProgressChanged;
-
-            // Find the time text view
-            var timeText = (TextView)pageView.FindViewById(Resource.Id.taskTimeText);
-
-            // Attach on-click event
-            timeText.Click += TimeText_Click;
-        }
-
-        /// <summary>
-        /// Fired when time text view is clicked
-        /// Shows time picker dialog to the user
-        /// </summary>
-        private void TimeText_Click(object sender, System.EventArgs e)
-        {
-            // If user didn't check constant time flag...
-            if (!CurrentViewModel.TaskHasConstantTime)
-                // We shouldn't do anything
-                return;
-
-            // Get currently shown time 
-            var currentTime = CurrentViewModel.TaskConstantTime;
-
-            // Create the dialog
-            var timePicker = new TimePickerDialog(this, this, currentTime.Hours, currentTime.Minutes, true);
-
-            // Show it to the user
-            timePicker.Show();
-        }
-
-
-        /// <summary>
-        /// Fired when priority seek bar's progress changes
-        /// It allows for manual binding, since Mvx one isn't working apparently
-        /// </summary>
-        private void PrioritySeekBar_ProgressChanged(object sender, DiscreteSeekBar.ProgressChangedEventArgs e)
-        {
-            // Get the seekbar itself
-            var seekBar = sender as DiscreteSeekBar;
-
-            // Set it's progress to view model 
-            CurrentViewModel.TaskPrioritySliderValue = seekBar.Progress;
-        }
-
-        /// <summary>
-        /// Fired whenever new time is being set in time picker dialog
-        /// </summary>
-        public void OnTimeSet(TimePicker view, int hourOfDay, int minute) => CurrentViewModel.TaskConstantTime = new TimeSpan(hourOfDay, minute, 0);
     }
 }
