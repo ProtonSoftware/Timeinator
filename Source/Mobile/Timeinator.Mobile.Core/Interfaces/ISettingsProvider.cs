ea3974ebecc648598cab02dfbfb46af3cb36cf7d
GummyBearStudio <keyboardwarriror@gmail.com>
2019-10-23 18:19:15 +0200

    Implemented SettingsProvider

---
+++ Source/Mobile/Timeinator.Mobile.Core/Interfaces/ISettingsProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Timeinator.Core;
+
+namespace Timeinator.Mobile.Domain
+{
+    /// <summary>
+    /// Interface for managing app settings at runtime
+    /// </summary>
+    public interface ISettingsProvider
+    {
+        #region Config
+
+        bool SessionWasFinishTime { get; }
+        int TimerTickRate { get; }
+        double MinimumTaskTime { get; }
+
+        #endregion
+
+        List<string> Languages { get; }
+        string LanguageValue { get; set; }
+        bool IsDarkModeOn { get; set; }
+        bool HighestPrioritySetAsFirst { get; set; }
+        bool RecalculateTasksAfterBreak { get; set; }
+
+        void SaveSetting(SettingsPropertyInfo setting);
+    }
+}
