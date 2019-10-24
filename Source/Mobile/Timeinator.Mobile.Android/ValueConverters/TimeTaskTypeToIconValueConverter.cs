b7edca4be643bc23df818a36993cd56df1732e89
Minorsonek <minorsonek@gmail.com>
2019-10-24 11:36:14 +0200

    Moved all the value converters to android project since literally all of them are used exclusively here and that allows for using android specific stuff

---
+++ Source/Mobile/Timeinator.Mobile.Android/ValueConverters/TimeTaskTypeToIconValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using MvvmCross.Converters;
+using System;
+using System.Globalization;
+using Timeinator.Mobile.Domain;
+
+namespace Timeinator.Mobile.Android
+{
+    /// <summary>
+    /// The value converter for task's type to get appropriate icon for
+    /// </summary>
+    public class TimeTaskTypeToIconValueConverter : IMvxValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var type = (TimeTaskType)value;
+
+            switch(type)
+            {
+                case TimeTaskType.Generic:
+                    return Resource.Drawable.icon_type_generic;
+
+                case TimeTaskType.Reading:
+                    return Resource.Drawable.icon_type_reading;
+
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
