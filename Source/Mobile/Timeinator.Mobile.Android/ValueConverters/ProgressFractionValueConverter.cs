264d01e49dfc03e14bcf4552efd435b49556609e
Minorsonek <minorsonek@gmail.com>
2019-10-27 19:05:43 +0100

    Improvements in UI for task list

---
+++ Source/Mobile/Timeinator.Mobile.Android/ValueConverters/ProgressFractionValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using MvvmCross.Converters;
+using System;
+using System.Globalization;
+
+namespace Timeinator.Mobile.Android
+{
+    /// <summary>
+    /// Converts provided progress to fraction string
+    /// </summary>
+    public class ProgressFractionValueConverter : IMvxValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var currentProgress = (double)value;
+            var maxProgress = (double)parameter;
+
+            return $"{currentProgress} / {maxProgress}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
